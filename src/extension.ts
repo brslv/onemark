7c1cf0b78f40eb4c17897cc09f2ed550ea7b3e34
Borislav Grigorov <bobi@noblehire.io>
2020-02-19 15:59:43 +0200

    set marks; add clear marks command

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,42 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.setMark', () => {
-		vscode.window.showInformationMessage('Hello World!');
-	});
+  let setMark = vscode.commands.registerCommand(
+    "extension.setMark",
+    async () => {
+      // current editor
+      const editor = vscode.window.activeTextEditor;
 
-	context.subscriptions.push(disposable);
+      // check if there is no selection
+      if (editor?.selection.isEmpty) {
+        const position = editor.selection.active;
+        const line = position.line;
+
+        vscode.window.showInformationMessage(`Line: ${line}`);
+
+        const linesFromState: Number[] = context.workspaceState.get(
+          "lines",
+          []
+        );
+        await context.workspaceState.update("lines", [
+          ...new Set([...linesFromState, line])
+        ]);
+      } else {
+        vscode.window.showInformationMessage(`Couldn't create a mark.`);
+      }
+
+      const savedLines = await context.workspaceState.get("lines");
+      console.log(savedLines);
+    }
+  );
+
+  let clearMarks = vscode.commands.registerCommand(
+    "extension.clearMarks",
+    async () => {
+      await context.workspaceState.update("lines", []);
+      vscode.window.showInformationMessage("All marks cleared successfully.");
+    }
+  );
+
+  context.subscriptions.push(setMark, clearMarks);
 }
